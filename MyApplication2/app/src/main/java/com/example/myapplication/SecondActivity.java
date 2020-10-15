1f367773d3c1174b94ead5e04af7780095285911
JinalGandhi07 <gandhi07@gmail.com>
2020-10-15 13:27:25 +0530

    Back button is added on the second Activity, Everything else was done already

--- MyApplication2/app/src/main/java/com/example/myapplication/SecondActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 private TextView textView;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        btn=findViewById(R.id.button2);
         textView=(TextView) findViewById(R.id.text);
         Intent i=getIntent();
         Bundle b=i.getExtras();
@@ -22,5 +26,19 @@ private TextView textView;
             textView.setText(j);
         }
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToSecondppage();
+            }
+        });
+
+    }
+
+    public  void moveToSecondppage(){
+        Intent intent=new Intent (SecondActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
